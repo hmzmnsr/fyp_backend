a78af9f40cd9a80709135bf130f3ef4e6e7022a4
Hamza <hamzamansoora@gmail.com>
2024-09-18 01:36:10 +0500

    initial commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import { userRouter } from "../routes/user.router.js";
 import { programRouter } from "../routes/program.route.js";
 import { downloadRouter } from "../routes/download.route.js";
 import { facultyRouter } from "../routes/faculty.route.js";
+import { albumRouter } from "../routes/gallery.route.js";
 
 const defineRoutes = (app) => {
   app.use('/api/users', userRouter);
   app.use('/api/programs', programRouter);
   app.use('/api/downloads', downloadRouter);
   app.use('/api/faculty', facultyRouter);
+  app.use('/api/gallery', albumRouter)
 };
 
 export { defineRoutes };
